709223d1c8c667cbbf561ac98d038945ab610746
Stephen Wright <ste.c.wr@gmail.com>
2016-01-15 13:32:54 +0000

    some stuff

--- app/components/info-module.js
+++ app/components/info-module.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     hasTitle: function() {
         return this.get('type') !== 'macro';
-    }.property('title')
+    }.property('title'),
+    click: function() {
+        Ember.$.get('/api/deptnotices');
+    }
 });
